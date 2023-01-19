4b8ed85064a45b2d5568bc6b016ba2d8f5912493
Tumaii <sdmm2112@gmail.com>
2023-01-18 21:21:17 -0500

    config port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+const port =process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
@@ -18,6 +19,4 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Servidor corriendo en puerto 3000')
-})
+app.listen(port,()=>console.log(`servidor escuchando en puerto ${port}`));
